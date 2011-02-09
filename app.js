0555bfad5dad6a266330e08fcefe07bdf8e9b584
Andy Vanee <1andyvanee@gmail.com>
2011-02-09 14:07:59 -0700

    Minor tweaks and README added

--- app.js
+++ app.js
@@ -79,6 +79,6 @@ var row = {
 }
 
 function replaceLinks(text) {
-  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
+  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
   return text.replace(exp,"<a href='$1'>$1</a>"); 
 }
